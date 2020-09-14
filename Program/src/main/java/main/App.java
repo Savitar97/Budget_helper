4178ae27498c84562f898c6bb7d562fbb10bf392
Savitar97 <atoth1571@gmail.com>
2020-09-14 20:45:26 +0200

    Main és App osztályok elkészítve valamint a pom.xml kibővítve

---
+++ Program/src/main/java/main/App.java
@@ -0,0 +1,14 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Budget_Helper");
+        stage.setResizable(false);
+
+    }
+}
