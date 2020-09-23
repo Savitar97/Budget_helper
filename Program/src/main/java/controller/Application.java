30c858ac4cb2dade8711ab4f3324e98b4959fcb8
Savitar97 <atoth1571@gmail.com>
2020-09-23 21:55:33 +0200

    Call DataDao from Application

--- Program/src/main/java/controller/Application.java
+++ Program/src/main/java/controller/Application.java
@@ -1,4 +1,26 @@
 package controller;
 
+import javafx.scene.input.MouseEvent;
+import result.DataDao;
+import result.model.DataModel;
+
+import java.io.IOException;
+import java.util.List;
+
 public class Application {
+
+    private DataDao dataDao;
+
+    public void initialize(){
+        dataDao=DataDao.getInstance();
+        updateTable();
+    }
+
+    public void updateTable(){
+        List<DataModel> dataList=dataDao.findAll();
+    }
+
+
+
+    public void returnLogin(MouseEvent mouseEvent) throws IOException {}
 }
