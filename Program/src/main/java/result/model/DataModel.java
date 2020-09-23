e0c2ed1d10ec8ec6a0337be42f9b77484ea6af94
kunanett <43445846+kunanett@users.noreply.github.com>
2020-09-23 15:38:33 +0200

    importok hozzáadása a DataModel osztályban

--- Program/src/main/java/result/model/DataModel.java
+++ Program/src/main/java/result/model/DataModel.java
@@ -1,4 +1,17 @@
 package result.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class DataModel {
 }
