9c1aa3d1474f472251c1169e4c179f04b61dfead
Savitar97 <atoth1571@gmail.com>
2020-09-23 22:33:12 +0200

    Application class updateTable method done

--- Program/src/main/java/controller/Application.java
+++ Program/src/main/java/controller/Application.java
@@ -1,13 +1,17 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import result.DataDao;
@@ -15,6 +19,7 @@ import result.model.DataModel;
 
 import java.io.IOException;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class Application {
@@ -35,7 +40,7 @@ public class Application {
     private TableColumn<DataModel, String> description;
     @FXML
     private TableColumn<DataModel, String> comment;
-    
+
     private DataDao dataDao;
 
     public void initialize(){
@@ -45,6 +50,37 @@ public class Application {
 
     public void updateTable(){
         List<DataModel> dataList=dataDao.findAll();
+
+        id.setCellValueFactory(new PropertyValueFactory<>("ID"));
+        username.setCellValueFactory(new PropertyValueFactory<>("Felhasználónév"));
+        created.setCellValueFactory(new PropertyValueFactory<>("Dátum"));
+        amount.setCellValueFactory(new PropertyValueFactory<>("Összeg"));
+        description.setCellValueFactory(new PropertyValueFactory<>("Leírás"));
+        comment.setCellValueFactory(new PropertyValueFactory<>("Komment"));
+
+        created.setCellFactory(column -> {
+            TableCell<DataModel, ZonedDateTime> cell = new TableCell<DataModel, ZonedDateTime>() {
+                private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd - HH:mm:ss Z");
+
+                @Override
+                protected void updateItem(ZonedDateTime item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if(empty) {
+                        setText(null);
+                    }
+                    else {
+                        setText(item.format(formatter));
+                    }
+                }
+            };
+
+            return cell;
+        });
+
+        ObservableList<DataModel> observableResult = FXCollections.observableArrayList();
+        observableResult.addAll(dataList);
+
+        dataTable.setItems(observableResult);
     }
 
     public void setUsername(String username) {
