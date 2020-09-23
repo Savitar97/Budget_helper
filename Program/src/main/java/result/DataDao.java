9f59ed6fed035d79957acd88c41c9e66dddeb814
petradam99 <petraadam70@gmail.com>
2020-09-23 21:30:28 +0200

    DataDao.java fájl hozzáadása a projekthez

---
+++ Program/src/main/java/result/DataDao.java
@@ -0,0 +1,11 @@
+package result;
+
+import jpa.GenericJPADao;
+import result.model.DataModel;
+
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class DataDao extends GenericJPADao<DataModel> {
+
+}
