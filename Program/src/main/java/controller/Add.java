323b98e913d400c0d087f600c310dff2e2521103
Savitar97 <atoth1571@gmail.com>
2020-09-23 22:41:54 +0200

    add fxml done

--- Program/src/main/java/controller/Add.java
+++ Program/src/main/java/controller/Add.java
@@ -1,4 +1,13 @@
 package controller;
 
+import javafx.scene.input.MouseEvent;
+
 public class Add {
+    public void addTable(MouseEvent mouseEvent) {
+
+    }
+
+    public void returnApp(MouseEvent mouseEvent) {
+
+    }
 }
