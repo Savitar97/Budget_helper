4f6d7d2ac3ddc0e18dd42e75346da6033e9c35b8
Savitar97 <atoth1571@gmail.com>
2020-09-23 22:30:09 +0200

    Application class setName method done and declared table from fxml

--- Program/src/main/java/controller/Application.java
+++ Program/src/main/java/controller/Application.java
@@ -1,19 +1,41 @@
 package controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import result.DataDao;
 import result.model.DataModel;
 
 import java.io.IOException;
+import java.time.ZonedDateTime;
 import java.util.List;
 
 public class Application {
 
+    @FXML
+    public Label userNameLabel;
+    @FXML
+    private TableColumn<DataModel, Long> id;
+    @FXML
+    private TableView<DataModel> dataTable;
+    @FXML
+    private TableColumn<DataModel, String> username;
+    @FXML
+    private TableColumn<DataModel, ZonedDateTime> created;
+    @FXML
+    private TableColumn<DataModel, Long> amount;
+    @FXML
+    private TableColumn<DataModel, String> description;
+    @FXML
+    private TableColumn<DataModel, String> comment;
+    
     private DataDao dataDao;
 
     public void initialize(){
@@ -26,7 +48,7 @@ public class Application {
     }
 
     public void setUsername(String username) {
-
+        userNameLabel.setText(username);
     }
 
 
