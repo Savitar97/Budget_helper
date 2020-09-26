3b6b52a9f9455eb7a50168bac045d57b1a039974
kunanett <43445846+kunanett@users.noreply.github.com>
2020-09-26 18:08:57 +0200

    Create Stat.java

---
+++ Program/src/main/java/controller/Stat.java
@@ -0,0 +1,16 @@
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import result.DataDao;
+
+import java.util.List;
+
+public class Stat {
+
+}
