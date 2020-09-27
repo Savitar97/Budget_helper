76561088d01dbc9e17c9795305d18748a69bf993
Savitar97 <atoth1571@gmail.com>
2020-09-27 23:17:01 +0200

    Add user icon and application image

--- Program/src/main/java/main/App.java
+++ Program/src/main/java/main/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -14,6 +15,7 @@ public class App extends Application {
         stage.setTitle("Budget_Helper");
         stage.setResizable(false);
         stage.setScene(new Scene(root));
+        stage.getIcons().add(new Image(getClass().getResource("/images/image.png").toExternalForm()));
         stage.show();
     }
 
